48f05eff6e9558c25cd5903a3dc179d2eca11610
Maria Salmasian <maria.salmasian@gmail.com>
2020-07-25 02:04:48 +0400

    -added the second stage of the program tic tac toe and some changes

--- GameTicTacToe/src/TicTacToeStageTwo.java
+++ GameTicTacToe/src/TicTacToeStageTwo.java
@@ -23,13 +23,19 @@ public class TicTacToeStageTwo {
     }
 
     protected void print() {
+        System.out.println("-------------");
+
         for (int i = 0; i < 3; i++) {
+            System.out.print("|  ");
             for (int j = 0; j < 3; j++) {
                 System.out.print(this.game[i][j] + "  ");
             }
+            System.out.print("|  ");
             System.out.println();
 
+
         }
+        System.out.println("-------------");
     }
 
     protected boolean gameIsWon() {
